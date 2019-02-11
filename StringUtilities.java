05db52ace4ac3c1c93f6388ed10db7c34a71c1dc
Nirmala <nirmala.talari@gmail.com>
2019-02-11 10:52:47 -0500

    Quiz1Submission-StringUtilities

--- StringUtilities.java
+++ StringUtilities.java
@@ -5,7 +5,7 @@ public class StringUtilities {
      * @return `input`
      */
     public String returnInput(String input) {
-        return null;
+        return input;
     }
 
     /**
@@ -14,7 +14,8 @@ public class StringUtilities {
      * @return the concatenation of `baseValue` and `valueToBeAdded`
      */
     public String concatenate(String baseValue, String valueToBeAdded) {
-        return null;
+        String conc = baseValue.concat(valueToBeAdded);
+        return conc;
     }
 
     /**
@@ -22,7 +23,14 @@ public class StringUtilities {
      * @return identical string with characters in opposite order
      */
     public String reverse(String valueToBeReversed) {
-        return null;
+        int i = 0;
+        int len = valueToBeReversed.length();
+        StringBuilder reverseStr = new StringBuilder(len);
+        for (i = len-1; i>=0; i--)
+        {
+            reverseStr.append(valueToBeReversed.charAt(i));
+        }
+        return reverseStr.toString();
     }
 
     /**
@@ -30,7 +38,16 @@ public class StringUtilities {
      * @return middle character of `word`
      */
     public Character getMiddleCharacter(String word) {
-        return null;
+        int position;
+        if (word.length() % 2 == 0)
+        {
+            position = word.length()/ 2-1;
+        }
+        else
+        {
+            position = word.length() / 2;
+        }
+        return word.charAt(position);
     }
 
     /**
@@ -39,7 +56,17 @@ public class StringUtilities {
      * @return `value` with char of value `charToRemove` removed
      */
     public String removeCharacter(String value, Character charToRemove) {
-        return null;
+        StringBuilder str = new StringBuilder();
+        int index = 0;
+        for (int i = 0; i<value.length(); i++)
+        {
+            if (value.charAt(i) != charToRemove)
+            {
+                str.append(value.charAt(i));
+            }
+        }
+
+        return str.toString();
     }
 
     /**
@@ -47,6 +74,7 @@ public class StringUtilities {
      * @return last `word` in sentence
      */
     public String getLastWord(String sentence) {
-        return null;
+        String arr[] = sentence.split(" ");
+        return arr[(arr.length)-1];
     }
 }
